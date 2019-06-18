4a3696b51a53cc9a11125826602cfb459fdbd9be
lamdimiya <cibomegastructure@gmail.com>
2019-06-18 21:52:49 +0300

    1

---
+++ test.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function() {
+    var $element = $('#bubble');
+    var phrases = [
+        'This is how bubbleText works.',
+        'Animating each letter in a friendly way',
+        'Thanks for seeing it :)',
+        'It really matters to me ...',
+        'Regards,',
+        'Guedes, Washington L.',
+    ];
+    var index = -1;
+    (function loopAnimation() {
+        index = (index + 1) % phrases.length;
+        bubbleText({
+            element: $element,
+            newText: phrases[index],
+            letterSpeed: 70,
+            callback: function() {
+                setTimeout(loopAnimation, 1000);
+            },
+        });
+    })();
+});
